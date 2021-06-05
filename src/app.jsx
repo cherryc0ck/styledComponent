2b5bd388264e20c35bc4407fa62058318acb27df
oriharaa <qstar9537@naver.com>
2021-06-05 22:08:08 +0900

    First Commit

---
+++ src/app.jsx
@@ -0,0 +1,27 @@
+import './app.css';
+import styled from 'styled-components';
+
+const Circle = styled.div`
+  width: 5rem;
+  height: 5rem;
+  background: black;
+  border-radius: 50%;
+`;
+
+const CircleTwo = styled.div`
+  width: 5rem;
+  height: 5rem;
+  background: ${props => props.color || 'black'};
+  border-radius: 50%;
+`;
+
+function App() {
+  return (
+    <>
+    <Circle />
+    <CircleTwo color="blue"/>
+    </>
+  );
+}
+
+export default App;
