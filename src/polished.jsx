4a9a2a7d9a1e22f4cae63b4e61d20cff04024741
oriharaa <qstar9537@naver.com>
2021-08-21 19:34:42 +0900

    Yarn add Polished

--- src/polished.jsx
+++ src/polished.jsx
@@ -1,7 +1,14 @@
 import React from 'react';
 import styled, { css } from 'styled-components';
-import Button from './components/ThemeProvider/Button';
+import Button from './components/button';
 
+const AppBlock = styled.div`
+  width:512px;
+  margin: 0 auto;
+  margin-top: 4rem;
+  border: 1px solid black;
+  padding: 1rem;
+`;
 
 function App() {
   return (
