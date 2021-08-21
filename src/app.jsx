734180d561bd7502695bfc5fd8b73a4bd376799f
oriharaa <qstar9537@naver.com>
2021-08-21 20:00:15 +0900

    Study ThemeProvider + Unstructured Button.js

--- src/app.jsx
+++ src/app.jsx
@@ -1,14 +1,34 @@
 import React from 'react';
-import styled, { css } from 'styled-components';
+import styled, { ThemeProvider } from 'styled-components';
 import Button from './components/ThemeProvider/Button';
+
+const AppBlock = styled.div`
+  width: 512px;
+  margin: 0 auto;
+  margin-top: 4rem;
+  border:1px solid black;
+  padding: 1rem;
+`
 
 
 function App() {
   return (
     <>
-      <AppBlock>
-        <Button>BUTTON</Button>
-      </AppBlock>
+     <ThemeProvider
+       theme={{
+         palette: {
+           blue: '#228be6',
+           gray: '#485057',
+           pink: '#f06595'
+         }
+       }} 
+     >
+       <AppBlock>
+         <Button>BUTTON</Button>
+         <Button color="gray">BUTTON</Button>
+         <Button color="pink">BUTTON</Button>
+       </AppBlock>
+     </ThemeProvider>
     </>
   )
 }
