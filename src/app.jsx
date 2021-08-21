8e579865c16e223f6f271b1402a45109dfd628eb
oriharaa <qstar9537@naver.com>
2021-08-21 20:58:59 +0900

    Add Dialog Component

--- src/app.jsx
+++ src/app.jsx
@@ -1,14 +1,16 @@
 import React from 'react';
 import styled, { ThemeProvider } from 'styled-components';
+import Dialog from './components/Dialog';
 import Button from './components/ThemeProvider/Button';
 
 const AppBlock = styled.div`
   width: 512px;
   margin: 0 auto;
   margin-top: 4rem;
-  border:1px solid black;
+  border: 1px solid black;
   padding: 1rem;
-`
+`;
+
 const ButtonGroup = styled.div`
   & + & {
     margin-top: 1rem;
@@ -44,7 +46,30 @@ function App() {
           <Button>BUTTON</Button>
           <Button color="pink" size="small">BUTTON</Button>
          </ButtonGroup>
+         <ButtonGroup>
+          <Button size="large" outline>BUTTON</Button>
+          <Button color="gray" outline>BUTTON</Button>
+          <Button color="pink" size="small" outline>BUTTON</Button>
+         </ButtonGroup>
+         <ButtonGroup>
+          <Button size="large" fullWidth>
+            BUTTON
+          </Button>
+          <Button size="large" color="gray" fullWidth>
+            BUTTON
+          </Button>
+          <Button size="large" color="pink" fullWidth>
+            BUTTON
+          </Button>
+        </ButtonGroup>
        </AppBlock>
+       <Dialog
+        title="정말로 삭제하시겠습니까?"
+        confirmText="삭제"
+        cancelText="취소"
+       >
+         데이터를 정말로 삭제하시겠습니까?
+       </Dialog>
      </ThemeProvider>
     </>
   )
